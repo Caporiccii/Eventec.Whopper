71c3c8df6fa5505570a740dbfd0c8cd87470766e
Caporiccii <dmarco792@gmail.com>
2020-10-01 23:41:18 -0300

    Novo Projeto

---
+++ Whopper/src/test/java/com/eventec/Whopper/WhopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventec.Whopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
